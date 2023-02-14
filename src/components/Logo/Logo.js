3098f7631940e1529bc5d9e22b2533d96ba06bb7
usamakhalid <usamakhalid1600@gmail.com>
2023-02-14 13:21:20 +0300

    add logo files in components with logo.png file, add logic to logo.js file with some  tachyons class

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Logo.css/';
+import logo from './logo.png';
+
+
+function Logo(){
+    return(
+        <div className="class to be added later">
+
+         <div>
+         <img  src={logo}  alt='ImageNotFound'/>   
+         </div>       
+
+        </div>
+
+    );
+
+}
+
+export default Logo;
